4714b079c791b39f6d54f17a4810e0592676b3cd
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2025-04-23 23:43:25 +0800

    feat: Added ProductCard Component and Updated the Product Section in Home Page.

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { motion, useAnimation } from 'framer-motion'
+
+const ProductCard = ({ title, price, image, onClick }) => {
+    const imageControls = useAnimation();
+
+    const imageVariants = {
+        hidden: { opacity: 0, y: -50, filter: "blur(10px)" },
+        visible: {
+            opacity: 1,
+            y: 0,
+            filter: "blur(0px)",
+            transition: { duration: 0.6, ease: "easeOut" }
+        },
+    };
+    return (
+        <motion.div
+            className='w-full bg-light text-black hover:bg-primary hover:text-white transition-all ease-out duration-300 cursor-pointer flex flex-col gap-4 justify-center py-12'
+            onClick={onClick}
+            variants={{
+                hidden: { opacity: 0, y: 50, filter: "blur(10px)" },
+                visible: { opacity: 1, y: 0, filter: "blur(0px)", transition: { duration: 0.4, ease: "easeOut" } }
+            }}
+            onAnimationComplete={() => {
+                imageControls.start("visible");
+            }}
+            onMouseEnter={() => imageControls.start({ y: -20 })}
+            onMouseLeave={() => imageControls.start("visible")}>
+
+            <motion.img
+                src={image}
+                alt={`${title}'s Image`}
+                initial="hidden"
+                variants={imageVariants}
+                animate={imageControls}
+                transition={{ duration: 0.4, ease: 'easeOut' }}
+                viewport={{ once: true }}
+            />
+
+            <div className='flex flex-col gap-2'>
+                <p className='uppercase text-sm font-medium leading-normal'>{title}</p>
+                <p className='text-[32px] font-bold leading-normal'>₱{price}.00</p>
+            </div>
+        </motion.div>
+    )
+}
+
+
+export default ProductCard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,52 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../components/Button'
 import Tabs from '../components/Tabs'
+import ProductCard from '../components/ProductCard'
+import { fetchProductByCategory } from '../services/productService'
 
 import HeroMain from '../assets/hero/hero-section-image.png'
 import FloatingLeft from '../assets/hero/floating-left-element.png'
 import FloatingRight from '../assets/hero/floating-right-element.png'
 
-const TabContent = [
-    {
-        label: 'Hot Coffee',
-        content: <p>Sample 1</p>,
-        onClick: () => console.log("Test")
-    },
-    {
-        label: 'Cold Coffee',
-        content: <p>Sample 2</p>
-    },
-]
-
 const Home = () => {
+
+    const [products, setProducts] = useState([]);
+    const [tab, setTab] = useState('Hot Coffee');
+
+    const TabContent = [
+        {
+            label: 'Hot Coffee',
+            content: <ProductContainer products={products} />,
+            onClick: () => setTab('Hot Coffee')
+        },
+        {
+            label: 'Cold Coffee',
+            content: <ProductContainer products={products} />,
+            onClick: () => setTab('Cold Coffee')
+        },
+    ];
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchProductByCategory(tab);
+                setProducts(data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+
+        fetchData();
+    }, [tab]);
+
     return (
         <>
             <section className='bg-light relative'>
                 <motion.img
-                    initial={{ opacity: 0, x: -150, filter: 'blur(10px)' }}
+                    initial={{ opacity: 0, x: -180, filter: 'blur(15px)' }}
                     animate={{ opacity: 1, x: 0, filter: 'blur(0px)' }}
-                    transition={{ duration: 0.8, ease: 'easeOut', }}
+                    transition={{ duration: 1, ease: 'easeOut', }}
                     src={FloatingLeft} className='absolute left-0 top-3' />
                 <div className='mx-auto max-w-6xl py-16 px-4 flex gap-20'>
                     <div className='flex-1 flex flex-col justify-center'>
@@ -51,7 +71,7 @@ const Home = () => {
                                 Shop Beans & Brews
 
                                 <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1.5 12.8334L5.83061 7.78099C6.2158 7.3316 6.2158 6.66848 5.83061 6.21909L1.5 1.16671" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                                    <path d="M1.5 12.8334L5.83061 7.78099C6.2158 7.3316 6.2158 6.66848 5.83061 6.21909L1.5 1.16671" stroke="white" strokeWidth="1.5" strokeLinecap="round" />
                                 </svg>
 
                             </Button>
@@ -67,11 +87,11 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section className='bg-white relative overflow-hidden'>
+            <section className='bg-white relative overflow-hidden pb-9'>
                 <motion.img
-                    initial={{ opacity: 0, x: 150, filter: 'blur(10px)' }}
+                    initial={{ opacity: 0, x: 180, filter: 'blur(15px)' }}
                     whileInView={{ opacity: 1, x: 0, filter: 'blur(0px)' }}
-                    transition={{ duration: 0.8, ease: 'easeOut', delay: 0.4 }}
+                    transition={{ duration: 1, ease: 'easeOut', delay: 0.4 }}
                     viewport={{ once: true }}
                     src={FloatingRight}
                     className='absolute right-0 top-12' />
@@ -80,6 +100,7 @@ const Home = () => {
                         initial={{ opacity: 0, y: 50, filter: 'blur(10px)' }}
                         whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
                         transition={{ duration: 1, ease: 'easeOut', delay: 0.4 }}
+                        viewport={{ once: true }}
                         className='text-[40px] text-black font-bold'>
                         Discover Your Perfect Brew
                     </motion.h2>
@@ -87,6 +108,7 @@ const Home = () => {
                         initial={{ opacity: 0, y: 50, filter: 'blur(10px)' }}
                         whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
                         transition={{ duration: 1, ease: 'easeOut', delay: 0.8 }}
+                        viewport={{ once: true }}
                         className='mt-8'>
                         <Tabs tabs={TabContent} />
                     </motion.div>
@@ -97,3 +119,34 @@ const Home = () => {
 }
 
 export default Home
+
+const ProductContainer = ({ products }) => {
+
+    if (!products || products.length === 0) {
+        return <p>No products found.</p>;
+    }
+
+    const limitedProducts = products.slice(0, 3);
+
+    return (
+        <motion.div
+            className='grid grid-cols-2 md:grid-cols-3 gap-6'
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.2 } }
+            }}>
+            {
+                limitedProducts.map((product, index) => (
+                    <ProductCard
+                        key={index}
+                        title={product?.name}
+                        image={product?.image_url}
+                        price={product?.price} />
+                ))
+            }
+        </motion.div>
+    )
+}
